9790c448be8766017bbe2bbb3356e73858275bd3
Ardjan-Aalberts <aaalbert@avans.nl>
2018-02-10 17:55:59 +0100

    Ordened stylesheet,

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -27,7 +27,7 @@
 
     <!-- Cookie alert -->
     <link href="application/assets/cookie-alert/cookiealert.css" rel="stylesheet">
-    <script src="application/assets/cookie-alert/cookiealert-standalone.js"></script>
+    <script src="application/assets/cookie-alert/cookiealert.js"></script>
     <script src="application/assets/cookie-alert/js-cookie.js"></script>
 
 </head>
@@ -46,35 +46,125 @@
 </div>
 
 <!-- NAVBAR -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark" id="menu">
     <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="index.html"><span class="icon-uilove icon-uilove-realestate"></span></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu-content" aria-controls="menu-content" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home
-                        <span class="sr-only">(current)</span>
+
+        <div class="collapse navbar-collapse" id="menu-content">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item ">
+                    <a class="nav-link" href="#" role="button">
+                        Home
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
+                <li class="nav-item ">
+                    <a class="nav-link" href="#" role="button">
+                        Contact
+                    </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Services</a>
+
+                <li class="nav-item dropdown megamenu">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Categorieen
+                    </a>
+                    <div class="dropdown-menu custom-padding">
+                            <div class="row justify-content-md-center no-margin">
+                                <div class="col col-md-8">
+                                    <div class="row">
+                                        <div class="col-md-6 col-lg-3">
+                                            <ul class="list-unstyled">
+                                                <li class="title">Homepage</li>
+                                                <li><a href="index.html">Homepage 1</a></li>
+                                                <li><a href="index2.html">Homepage 2</a></li>
+                                                <li><a href="index3.html">Homepage 3</a></li>
+                                                <li><a href="index4.html">Homepage 4</a></li>
+                                                <li><a href="index5.html">Homepage 5</a></li>
+                                                <li><a href="index6.html">Homepage 6</a></li>
+                                                <li><a href="index7.html">Homepage 7</a></li>
+                                                <li class="title">Login Pages</li>
+                                                <li><a href="signin.html">Signin</a></li>
+                                                <li><a href="register.html">Register</a></li>
+                                                <li><a href="forgot-password.html">Forgot Password</a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-md-6 col-lg-3">
+                                            <ul class="list-unstyled">
+                                                <li class="title">Property Listing</li>
+                                                <li><a href="property_listing.html">List View</a></li>
+                                                <li><a href="property_grid.html">Grid View</a></li>
+                                                <li><a href="property_listing_map.html">Map View</a></li>
+                                                <li class="title">Single Property</li>
+                                                <li><a href="property_single.html">Single View 1</a></li>
+                                                <li><a href="property_single2.html">Single View 2</a></li>
+                                                <li><a href="property_single3.html">Single View 3</a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-md-6 col-lg-3">
+                                            <ul class="list-unstyled">
+                                                <li class="title">Other Pages</li>
+                                                <li><a href="plans.html">Plans</a></li>
+                                                <li><a href="information_page.html">Information Page</a></li>
+                                                <li><a href="coming_soon.html">Coming Soon</a></li>
+                                                <li><a href="404_error.html">Error Page</a></li>
+                                                <li><a href="success.html">Success Page</a></li>
+                                                <li><a href="contact.html">Contact Page</a></li>
+                                                <li><a href="compare.html">Compare Properties</a></li>
+                                                <li class="title">Agent Pages</li>
+                                                <li><a href="agent_list.html">Agent List</a></li>
+                                                <li><a href="agent.html">Agent Profile</a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-md-6 col-lg-3">
+                                            <ul class="list-unstyled">
+                                                <li class="title">Account Pages</li>
+                                                <li><a href="my_listing_add.html">Add Listing</a></li>
+                                                <li><a href="my_bookmarked_listings.html">Bookmarked Listing</a></li>
+                                                <li><a href="my_listings.html">My Listings</a></li>
+                                                <li><a href="my_profile.html">My Profile</a></li>
+                                                <li><a href="my_password.html">Change Password</a></li>
+                                                <li><a href="my_notifications.html">Notifications</a></li>
+                                                <li><a href="my_membership.html">Membership</a></li>
+                                                <li><a href="my_payments.html">Payments</a></li>
+                                                <li><a href="my_account.html">Account</a></li>
+                                                <li class="title">Blog Pages</li>
+                                                <li><a href="blog.html">Blog Archive</a></li>
+                                                <li><a href="blog_single.html">Blog Single</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
+
             </ul>
+
+            <ul class="navbar-nav ml-auto">
+
+                <li class="nav-item dropdown user-account">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Profiel
+                    </a>
+                    <div class="dropdown-menu">
+                        <a href="my_profile.html" class="dropdown-item">My Profile</a>
+                        <a href="my_password.html" class="dropdown-item">Change Password</a>
+                        <a href="my_notifications.html" class="dropdown-item">Notifications</a>
+                        <a href="my_membership.html" class="dropdown-item">Membership</a>
+                        <a href="my_payments.html" class="dropdown-item">Payments</a>
+                        <a href="my_account.html" class="dropdown-item">Account</a>
+                    </div>
+                </li>
+                <li class="nav-item"><a class="nav-link nav-btn" href="my_listing_add.html"><span><i class="fa fa-shopping-cart" aria-hidden="true"></i> Winkelwagen</span></a></li>
+            </ul>
+
         </div>
     </div>
 </nav>
 
-<div class="col-lg-10 col-md-10 offset-lg-1 offset-md-1">
+<div class="col-lg-10 col-md-10 offset-lg-1 offset-md-1 mt-5">
     <div class="row">
         <div class="col-12">
             <div class="banner-container background-cover layout-fill layout-align-center-center layout-row"
