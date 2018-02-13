df578744c022c7a99aa856a40fc3206c4f3f6b6c
Ardjan-Aalberts <aaalbert@avans.nl>
2018-02-13 15:06:48 +0100

    CSS changes, cart responsive, sidebar responsive

--- application/views/cart/index.php
+++ application/views/cart/index.php
@@ -10,15 +10,40 @@
 <div class="row">
 
     <!-- SIDEBAR -->
-    <div class="col-lg-3 col-md-3" style="border-right: 1px solid #215678; margin-top: 1.5rem;">
+    <div class="col-lg-3 col-md-3 sidebar" style="border-right: 1px solid #215678; margin-top: 1.5rem;">
         <h3 class="partial-title" style="">Uitgelicht</h3>
-        <div class="jumbotron custom-margin">
-            WIP
+        <div class="custom-margin">
+            <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <div class="card-body">
+                    <h4 class="card-title">
+                        <a href="#">Item Four</a>
+                    </h4>
+                    <h5>$24.99</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
+                        numquam
+                        aspernatur!</p>
+                </div>
+                <div class="card-footer">
+                    <div class="row">
+                        <div class="reviews_product col-10">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="far fa-star"></i>
+                        </div>
+                        <div class="col-2">
+                            <button class="btn heart-button float-right"><i class="far fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
     <!-- PRODUCTS -->
-    <div class="col-lg-9 col-md-9">
+    <div class="col-xl-9 col-lg-12 col-md-12">
 
         <div class="row custom-margin">
             <div class="col-12">
@@ -28,12 +53,12 @@
 
         <div class="row" style="margin-top: 1rem">
             <div class="col-12">
-                <table id="cart" class="table table-hover table-condensed">
+                <table id="cart" class="table table-hover table-condensed" style="background: whitesmoke;">
                     <thead>
                     <tr>
-                        <th style="width:40%">Product</th>
+                        <th style="width:30%">Product</th>
                         <th style="width:15%">Prijs</th>
-                        <th style="width:20%">Aantal</th>
+                        <th style="width:30%">Aantal</th>
                         <th style="width:15%" class="text-center">Subtotaal</th>
                         <th style="width:10%"></th>
                     </tr>
@@ -45,7 +70,7 @@
                                 <div class="col-sm-2 hidden-xs"><img src="http://placehold.it/100x100" alt="..."
                                                                      class="img-responsive"/></div>
                                 <div class="col-sm-10">
-                                    <h4 style="margin-top: 0.5rem !important;">Product 1</h4>
+                                    <h5 style="margin-top: 0.5rem !important;">Product 1</h5>
                                 </div>
                             </div>
                         </td>
@@ -121,7 +146,7 @@
                                 <div class="col-sm-2 hidden-xs"><img src="http://placehold.it/100x100" alt="..."
                                                                      class="img-responsive"/></div>
                                 <div class="col-sm-10">
-                                    <h4 style="margin-top: 0.5rem !important;">Product 2</h4>
+                                    <h5 style="margin-top: 0.5rem !important;">Product 2</h5>
                                 </div>
                             </div>
                         </td>
