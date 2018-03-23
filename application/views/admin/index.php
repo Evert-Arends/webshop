9bf7278bea3638bb8dac1fb1f41dd2e1ab2a8476
Ardjan-Aalberts <aaalbert@avans.nl>
2018-03-23 16:34:22 +0100

    Changes in styling

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -21,19 +21,52 @@
                 </div>
             </div>
             <div class="col-xl-3 col-sm-6 mb-3">
-            <div class="card text-white bg-danger o-hidden h-100">
-                <div class="card-body">
-                    <div class="card-body-icon">
-                        <i class="fa fa-fw fa-user"></i>
+                <div class="card text-white bg-success o-hidden h-100">
+                    <div class="card-body">
+                        <div class="card-body-icon">
+                            <i class="fa fa-fw fa-shopping-cart"></i>
+                        </div>
+                        <div class="mr-5">Producten</div>
                     </div>
-                    <div class="mr-5">username</div>
+                    <a class="card-footer text-white clearfix small z-1" href="/webshop/products">
+                        <span class="float-left">Bekijk producten</span>
+                        <span class="float-right">
+                            <i class="fa fa-angle-right"></i>
+                        </span>
+                    </a>
                 </div>
-                <a class="card-footer text-white clearfix small z-1" href="/webshop/logout">
-                    <span class="float-left">Uitloggen</span>
-                    <span class="float-right">
-                <i class="fa fa-angle-right"></i>
-              </span>
-                </a>
+            </div>
+            <div class="col-xl-3 col-sm-6 mb-3">
+                <div class="card text-white bg-warning o-hidden h-100">
+                    <div class="card-body">
+                        <div class="card-body-icon">
+                            <i class="fa fa-fw fa-list"></i>
+                        </div>
+                        <div class="mr-5">Klanten</div>
+                    </div>
+                    <a class="card-footer text-white clearfix small z-1" href="/webshop/users">
+                        <span class="float-left">Bekijk klanten</span>
+                        <span class="float-right">
+                            <i class="fa fa-angle-right"></i>
+                        </span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 mb-3">
+                <div class="card text-white bg-danger o-hidden h-100">
+                    <div class="card-body">
+                        <div class="card-body-icon">
+                            <i class="fa fa-fw fa-user"></i>
+                        </div>
+                        <div class="mr-5">username</div>
+                    </div>
+                    <a class="card-footer text-white clearfix small z-1" href="/webshop/logout">
+                        <span class="float-left">Uitloggen</span>
+                        <span class="float-right">
+                            <i class="fa fa-angle-right"></i>
+                        </span>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
