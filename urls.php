e4d216b2fd7c7f39197a30074720f25f80de0610
Evert-Arends <evert-arends@hotmail.nl>
2018-03-23 08:14:05 +0100

    Doc change

--- urls.php
+++ urls.php
@@ -11,13 +11,16 @@
  *                      ROUTES                     *
  **************************************************/
 
-// Name of route ( BETA! ), and controller used to produce content.
-// Single point of entry, no longer supports method calls directly.
-// Route:Controller
-// Keep in mind that the underscore at the start of a route is reserved for the framework, ex: _route, _index etc.
-
-// Route name: Controller: Protected: (true / false).
-// ?Need to setup page model?
+/***
+ * If protected is false, level can be removed from array, because there's no need to check if the user has the correct
+ * level.
+ *
+ * Level 0 means that everyone who is logged in can access the page, people who are not logged in will be redirected to
+ * the login page.
+ *
+ * If there's a faulty route / non-existing controller the app will crash, and not allow anyone to access any page to
+ * prevent errors.
+ */
 
 
 $routes = array(
